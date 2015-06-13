2106bed772a2c79603f8400a243b6263071ca19b
Kris Immig <kris.immig@gmail.com>
2015-06-13 23:18:03 +0200

    Try different font fix.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,6 +26,8 @@ SimpleCms::Application.configure do
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
+
+
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
@@ -61,6 +63,7 @@ SimpleCms::Application.configure do
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
   config.assets.precompile += %w( vendor/modernizr.js )
+  config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
